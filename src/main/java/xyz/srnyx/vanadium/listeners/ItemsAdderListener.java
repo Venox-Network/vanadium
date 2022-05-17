74d5cbfa80a4b5c288a1c9d949c52c4191283e70
srnyx <25808801+srnyx@users.noreply.github.com>
2022-05-16 23:59:56 -0400

    Added `vanadium_crossbow` ability
    I also reorganized ItemsAdderListener.java to be more understandable and easier to edit

--- src/main/java/xyz/srnyx/vanadium/listeners/ItemsAdderListener.java
+++ src/main/java/xyz/srnyx/vanadium/listeners/ItemsAdderListener.java
@@ -1,6 +1,7 @@
 package xyz.srnyx.vanadium.listeners;
 
 import dev.lone.itemsadder.api.CustomEntity;
+import dev.lone.itemsadder.api.CustomStack;
 
 import org.bukkit.*;
 import org.bukkit.block.BlockFace;
@@ -10,9 +11,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.CrossbowMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -37,122 +40,143 @@ public class ItemsAdderListener implements Listener {
     public void interact(PlayerInteractEvent event) {
         final Player player = event.getPlayer();
         final ItemsAdderManager iam = new ItemsAdderManager(player);
+        final boolean sneaking = player.isSneaking();
+        final boolean cooldown = iam.notOnCooldown();
         final boolean leftClick = event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK;
         final boolean rightClick = event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK;
 
-        if (player.isSneaking()) {
-            if (iam.notOnCooldown()) {
-                // nyx_wand
-                if (leftClick && iam.holdingItem(false, "nyx_wand")) {
-                    iam.cooldown();
-                    ItemsAdderManager.cooldowns.put(player.getUniqueId(), System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(Main.config.getInt("custom-items.cooldown")));
-                    iam.durability(false, 2);
+        final long time = System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(Main.config.getInt("custom-items.cooldown"));
 
-                    // TNT item
-                    final Item tnt = player.getWorld().dropItem(player.getEyeLocation(), new ItemStack(Material.TNT));
-                    tnt.setVelocity(player.getEyeLocation().getDirection().multiply(1.5)); // Makes the TNT item move forward
-                    tnt.setPickupDelay(32767); // Make it so the TNT item can't be picked up
-                    tnt.setCustomNameVisible(true);
-                    tnt.setInvulnerable(true);
-                    tnt.setGlowing(true);
-                    // TNT item - Name
-                    final Map<Item, Long> timer = new ConcurrentHashMap<>();
-                    timer.put(tnt, System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(Main.config.getInt("custom-items.nyx_wand.tnt.fuse")));
-                    final BukkitRunnable runnable = new BukkitRunnable() {
-                        public void run() {
-                            tnt.setCustomName(ChatColor.RED + "" + ChatColor.BOLD + (TimeUnit.MILLISECONDS.toSeconds(timer.get(tnt) - System.currentTimeMillis()) + 1));
-                            tnt.getWorld().spawnParticle(Particle.END_ROD, tnt.getLocation(), 1, 0, 0, 0);
-                        }
-                    };
-                    runnable.runTaskTimer(Main.plugin, 0, 20);
+        // nyx_wand
+        if (iam.holdingItem(false, "nyx_wand") && leftClick && sneaking && cooldown) {
+            iam.cooldown();
+            ItemsAdderManager.cooldowns.put(player.getUniqueId(), time);
+            iam.durability(false, 2);
 
-                    // Creeper
-                    new BukkitRunnable() {
-                        public void run() {
-                            timer.remove(tnt);
-                            tnt.remove();
-                            runnable.cancel();
-                            final Creeper creeper = (Creeper) tnt.getWorld().spawnEntity(tnt.getLocation(), EntityType.CREEPER);
-                            creeper.setCustomName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "SRNYX BOMB");
-                            creeper.setExplosionRadius(2);
-                            creeper.setCustomNameVisible(false);
-                            creeper.setInvisible(true);
-                            creeper.setPowered(true);
-                            creeper.setSilent(true);
-                            creeper.setAI(false);
-                            creeper.explode();
-                        }
-                    }.runTaskLater(Main.plugin, Main.config.getInt("custom-items.nyx_wand.tnt.fuse") * 20L);
+            // TNT item
+            final Item tnt = player.getWorld().dropItem(player.getEyeLocation(), new ItemStack(Material.TNT));
+            tnt.setVelocity(player.getEyeLocation().getDirection().multiply(1.5)); // Makes the TNT item move forward
+            tnt.setPickupDelay(32767); // Make it so the TNT item can't be picked up
+            tnt.setCustomNameVisible(true);
+            tnt.setInvulnerable(true);
+            tnt.setGlowing(true);
+
+            final int fuse = Main.config.getInt("custom-items.nyx_wand.tnt.fuse");
+
+            // TNT item - Name
+            final Map<Item, Long> timer = new ConcurrentHashMap<>();
+            timer.put(tnt, System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(fuse));
+            // Decimal formatting stuff
+            final BukkitRunnable runnable = new BukkitRunnable() {
+                public void run() {
+                    tnt.setCustomName(ChatColor.RED + "" + ChatColor.BOLD + Math.round((timer.get(tnt) - System.currentTimeMillis()) / 100.0) / 10.0);
                 }
+            };
+            runnable.runTaskTimer(Main.plugin, 0, 1);
 
-                // chris_shield
-                if (rightClick && iam.holdingItem(true, "chris_shield")) {
-                    iam.cooldown();
-                    ItemsAdderManager.cooldowns.put(player.getUniqueId(), System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(Main.config.getInt("custom-items.cooldown")));
-                    iam.durability(true, 6);
+            // Creeper
+            new BukkitRunnable() {
+                public void run() {
+                    // Remove the TNT item from timer map
+                    timer.remove(tnt);
+                    // "Kill" the TNT item
+                    tnt.remove();
+                    // Cancel name changing runnable
+                    runnable.cancel();
 
-                    player.getWorld().spawnParticle(Particle.EXPLOSION_NORMAL, player.getLocation(), 20, 1, 1, 1);
+                    // Summon creeper
+                    final Creeper creeper = (Creeper) tnt.getWorld().spawnEntity(tnt.getLocation(), EntityType.CREEPER);
+                    creeper.setCustomName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "SRNYX BOMB");
+                    creeper.setExplosionRadius(2);
+                    creeper.setExplosionRadius(4);
+                    creeper.setCustomNameVisible(false);
+                    creeper.setInvisible(true);
+                    creeper.setSilent(true);
+                    creeper.setAI(false);
+                    creeper.explode();
+                }
+            }.runTaskLater(Main.plugin, fuse * 20L);
+        }
 
-                    // Damage entities
-                    final World world = player.getWorld();
-                    for (final Entity entity : world.getEntities()) {
-                        if (entity instanceof LivingEntity entityLiving && entityLiving != player && entityLiving.hasLineOfSight(player)) {
-                            if (entityLiving.getLocation().distance(player.getLocation()) <= Main.config.getInt("custom-items.chris_shield.damage.range")) {
-                                if (entityLiving instanceof Player livingPlayer) {
-                                    if (!livingPlayer.isBlocking()) DamageManager.damage(entityLiving, Main.config.getInt("custom-items.chris_shield.damage.amount"));
-                                } else DamageManager.damage(entityLiving, Main.config.getInt("custom-items.chris_shield.damage.amount"));
+        // chris_shield
+        if (iam.holdingItem(true, "chris_shield") && rightClick && sneaking && cooldown) {
+            iam.cooldown();
+            ItemsAdderManager.cooldowns.put(player.getUniqueId(), time);
+            iam.durability(true, 6);
 
-                                entityLiving.getWorld().spawnParticle(Particle.VILLAGER_ANGRY, entityLiving.getLocation().add(0, 1.5, 0), 1, 0, 0, 0);
-                            }
-                        }
+            player.getWorld().spawnParticle(Particle.EXPLOSION_NORMAL, player.getLocation(), 20, 1, 1, 1);
+
+            // Damage entities
+            final World world = player.getWorld();
+            for (final Entity entity : world.getEntities()) {
+                if (entity instanceof LivingEntity entityLiving && entityLiving != player && entityLiving.hasLineOfSight(player)) {
+                    if (entityLiving.getLocation().distance(player.getLocation()) <= Main.config.getInt("custom-items.chris_shield.damage.range")) {
+                        if (entityLiving instanceof Player livingPlayer) {
+                            if (!livingPlayer.isBlocking())
+                                DamageManager.damage(entityLiving, Main.config.getInt("custom-items.chris_shield.damage.amount"));
+                        } else
+                            DamageManager.damage(entityLiving, Main.config.getInt("custom-items.chris_shield.damage.amount"));
+
+                        entityLiving.getWorld().spawnParticle(Particle.VILLAGER_ANGRY, entityLiving.getLocation().add(0, 1.5, 0), 1, 0, 0, 0);
                     }
-                }
-            }
-
-            // chris_axe
-            if (iam.holdingItem(false, "chris_axe")) {
-                // Lightning
-                if (leftClick && iam.notOnCooldown()) {
-                    player.getWorld().strikeLightning(player.getTargetBlock(null, Main.config.getInt("custom-items.chris_axe.lightning.range")).getLocation());
-                    iam.durability(false, 4);
-                }
-
-                // Throw
-                if (rightClick) {
-                    int slot = player.getInventory().getHeldItemSlot();
-
-                    // Remove axe
-                    ItemsAdderManager.axes.put(player.getUniqueId(), player.getInventory().getItemInMainHand().clone());
-                    player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
-
-                    // Spawn arrow
-                    final Location eye = player.getEyeLocation();
-                    final Arrow arrow = player.getWorld().spawnArrow(eye, eye.getDirection(), Main.config.getInt("custom-items.chris_axe.throw.speed"), 0);
-                    arrow.setDamage(Main.config.getInt("custom-items.chris_axe.throw.damage"));
-                    arrow.setCustomName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "AXE");
-                    arrow.setCustomNameVisible(true);
-                    arrow.setPersistent(true);
-                    arrow.setShooter(player);
-                    arrow.setGlowing(true);
-
-                    // Remove arrow after a few seconds
-                    new BukkitRunnable() {
-                        public void run() {
-                            if (arrow.isValid()) iam.axe(arrow, slot);
-                        }
-                    }.runTaskLater(Main.plugin, Main.config.getInt("custom-items.chris_axe.throw.return") * 20L);
                 }
             }
         }
 
+        // chris_axe
+        if (iam.holdingItem(false, "chris_axe")) {
+            // Lightning
+            if (leftClick && cooldown) {
+                player.getWorld().strikeLightning(player.getTargetBlock(null, Main.config.getInt("custom-items.chris_axe.lightning.range")).getLocation());
+                iam.durability(false, 4);
+            }
+
+            // Throw
+            if (rightClick) {
+                // Remove axe
+                ItemsAdderManager.axes.put(player.getUniqueId(), player.getInventory().getItemInMainHand().clone());
+                player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+
+                // Spawn arrow
+                final Location eye = player.getEyeLocation();
+                final Arrow arrow = player.getWorld().spawnArrow(eye, eye.getDirection(), Main.config.getInt("custom-items.chris_axe.throw.speed"), 0);
+                arrow.setDamage(Main.config.getInt("custom-items.chris_axe.throw.damage"));
+                arrow.setCustomName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "AXE");
+                arrow.setCustomNameVisible(true);
+                arrow.setPersistent(true);
+                arrow.setShooter(player);
+                arrow.setGlowing(true);
+
+                // Remove arrow after a few seconds
+                final int slot = player.getInventory().getHeldItemSlot();
+                new BukkitRunnable() {
+                    public void run() {
+                        if (arrow.isValid()) iam.axe(arrow, slot);
+                    }
+                }.runTaskLater(Main.plugin, Main.config.getInt("custom-items.chris_axe.throw.return") * 20L);
+            }
+        }
+
         // nyx_dragon_spawn_egg
-        if (iam.holdingItem(false, "nyx_dragon_spawn_egg") && rightClick && event.getClickedBlock() != null) {
+        if (iam.holdingItem(false, "nyx_dragon_spawn_egg") && event.getClickedBlock() != null && rightClick) {
             final BlockFace blockFace = event.getBlockFace();
             final Location loc = event.getClickedBlock().getLocation().add(blockFace.getModX(), blockFace.getModY(), blockFace.getModZ());
             CustomEntity.spawn("vanadium:nyx_dragon", loc);
 
             // Remove egg if they are not in creative mode
             if (player.getGameMode() != GameMode.CREATIVE) player.getInventory().setItemInMainHand(new ItemStack(Material.AIR));
+        }
+
+        // vanadium_crossbow
+        if (iam.holdingItem(true, "vanadium_crossbow") && cooldown && player.getInventory().getItemInMainHand().getItemMeta() instanceof CrossbowMeta cb) {
+            if (cb.hasChargedProjectiles() && rightClick) {
+                // Summon pearl
+                final Location loc = player.getEyeLocation().add(player.getLocation().getDirection());
+                final EnderPearl pearl = (EnderPearl) player.getWorld().spawnEntity(loc, EntityType.ENDER_PEARL);
+                pearl.setVelocity(player.getEyeLocation().getDirection().multiply(Main.config.getDouble("custom-items.vanadium_crossbow.speed")));
+                pearl.setPersistent(true);
+                pearl.setShooter(player);
+            }
         }
     }
 
@@ -163,7 +187,7 @@ public class ItemsAdderListener implements Listener {
     public void damageEntity(EntityDamageByEntityEvent event) {
         if (event.getEntity() instanceof Player victim && event.getDamager() instanceof LivingEntity attacker) {
             final ItemsAdderManager iam = new ItemsAdderManager(victim);
-            if (victim.isBlocking() && iam.holdingItem(true, "chris_shield")) {
+            if (iam.holdingItem(true, "chris_shield") && victim.isBlocking()) {
                 if (new Random().nextInt(5) == 0) {
                     attacker.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, Main.config.getInt("custom-items.chris_shield.block.duration") * 20, 0));
                     attacker.getWorld().spawnParticle(Particle.VILLAGER_ANGRY, attacker.getLocation().add(0, 1.5, 0), 1, 0, 2, 0, 0);
@@ -180,6 +204,19 @@ public class ItemsAdderListener implements Listener {
                                 .send(attackerPlayer);
                     }
                 } else iam.durability(true, 1);
+            }
+        }
+    }
+
+    /**
+     * Called when an entity shoots something
+     */
+    @EventHandler
+    public void shoot(EntityShootBowEvent event) {
+        if (event.getBow() != null) {
+            CustomStack custom = CustomStack.byItemStack(event.getBow());
+            if (custom != null && custom.getId().equals("vanadium_crossbow")) {
+                event.setCancelled(true);
             }
         }
     }
