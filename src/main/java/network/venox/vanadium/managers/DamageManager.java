d5ef710476e27360466a29c79339e461b35635b2
srnyx <25808801+srnyx@users.noreply.github.com>
2022-06-19 18:34:37 -0400

    Improved `DamageManager.java` for Vanadium armor

--- src/main/java/network/venox/vanadium/managers/DamageManager.java
+++ src/main/java/network/venox/vanadium/managers/DamageManager.java
@@ -2,14 +2,17 @@ package network.venox.vanadium.managers;
 
 import dev.lone.itemsadder.api.CustomStack;
 
+import network.venox.vanadium.Main;
+
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.EntityEquipment;
 
 
 public class DamageManager {
+    private final boolean ia = Main.itemsAdderInstalled();
+
     //TODO: Account for enchantments
     /**
      * Damages an entity and accounts for armor
@@ -22,11 +25,12 @@ public class DamageManager {
         if (inv == null) return;
         double red = 0.0;
 
-        red += shield(inv, entity);
-        red += helmet(inv);
-        red += chestplate(inv);
-        red += leggings(inv);
-        red += boots(inv);
+        final DamageManager dm = new DamageManager();
+        red += dm.shield(inv, entity);
+        red += dm.helmet(inv);
+        red += dm.chestplate(inv);
+        red += dm.leggings(inv);
+        red += dm.boots(inv);
 
         entity.damage(damage - (damage * red));
     }
@@ -39,7 +43,7 @@ public class DamageManager {
      *
      * @return          The amount of damage reduction
      */
-    private static double shield(EntityEquipment inv, Entity entity) {
+    private double shield(EntityEquipment inv, LivingEntity entity) {
         if (inv.getItemInMainHand().getType() != Material.SHIELD && inv.getItemInOffHand().getType() != Material.SHIELD) return 0.0;
         if (!(entity instanceof Player player) || player.isBlocking()) return 0.05;
         return 0.0;
@@ -52,11 +56,15 @@ public class DamageManager {
      *
      * @return      The amount of damage reduction
      */
-    private static double helmet(EntityEquipment inv) {
+    private double helmet(EntityEquipment inv) {
         if (inv.getHelmet() == null) return 0.0;
 
+        if (ia) {
+            final CustomStack custom = CustomStack.getInstance("vanadium_helmet");
+            if (custom != null && inv.getHelmet() == custom.getItemStack()) return 0.16;
+        }
+
         final Material material = inv.getHelmet().getType();
-        if (inv.getHelmet() == CustomStack.getInstance("vanadium_helmet").getItemStack()) return 0.16;
         if (material == Material.DIAMOND_HELMET || material == Material.NETHERITE_HELMET) return 0.12;
         if (material == Material.GOLDEN_HELMET || material == Material.CHAINMAIL_HELMET || material == Material.IRON_HELMET || material == Material.TURTLE_HELMET) return 0.08;
         if (material == Material.LEATHER_HELMET) return 0.04;
@@ -71,11 +79,15 @@ public class DamageManager {
      *
      * @return      The amount of damage reduction
      */
-    private static double chestplate(EntityEquipment inv) {
+    private double chestplate(EntityEquipment inv) {
         if (inv.getChestplate() == null) return 0.0;
 
+        if (ia) {
+            final CustomStack custom = CustomStack.getInstance("vanadium_chestplate");
+            if (custom != null && inv.getChestplate() == custom.getItemStack()) return 0.36;
+        }
+
         final Material material = inv.getChestplate().getType();
-        if (inv.getChestplate() == CustomStack.getInstance("vanadium_chestplate").getItemStack()) return 0.36;
         if (material == Material.DIAMOND_CHESTPLATE || material == Material.NETHERITE_CHESTPLATE) return 0.32;
         if (material == Material.GOLDEN_CHESTPLATE || material == Material.CHAINMAIL_CHESTPLATE || material == Material.IRON_CHESTPLATE) return 0.20;
         if (material == Material.LEATHER_CHESTPLATE) return 0.12;
@@ -90,11 +102,15 @@ public class DamageManager {
      *
      * @return      The amount of damage reduction
      */
-    private static double leggings(EntityEquipment inv) {
-        if (inv.getLeggings() != null) return 0.0;
+    private double leggings(EntityEquipment inv) {
+        if (inv.getLeggings() == null) return 0.0;
+
+        if (ia) {
+            final CustomStack custom = CustomStack.getInstance("vanadium_leggings");
+            if (custom != null && inv.getLeggings() == custom.getItemStack()) return 0.28;
+        }
 
         final Material material = inv.getLeggings().getType();
-        if (inv.getLeggings() == CustomStack.getInstance("vanadium_leggings").getItemStack()) return 0.28;
         if (material == Material.DIAMOND_LEGGINGS || material == Material.NETHERITE_LEGGINGS) return 0.24;
         if (material == Material.IRON_LEGGINGS) return 0.20;
         if (material == Material.CHAINMAIL_LEGGINGS) return 0.16;
@@ -111,11 +127,15 @@ public class DamageManager {
      *
      * @return      The amount of damage reduction
      */
-    private static double boots(EntityEquipment inv) {
+    private double boots(EntityEquipment inv) {
         if (inv.getBoots() == null) return 0.0;
 
+        if (ia) {
+            final CustomStack custom = CustomStack.getInstance("vanadium_boots");
+            if (custom != null && inv.getBoots() == custom.getItemStack()) return 0.16;
+        }
+
         final Material material = inv.getBoots().getType();
-        if (inv.getBoots() == CustomStack.getInstance("vanadium_boots").getItemStack()) return 0.16;
         if (material == Material.DIAMOND_BOOTS || material == Material.NETHERITE_BOOTS) return 0.12;
         if (material == Material.IRON_BOOTS) return 0.08;
         if (material == Material.LEATHER_BOOTS || material == Material.GOLDEN_BOOTS || material == Material.CHAINMAIL_BOOTS) return 0.04;
